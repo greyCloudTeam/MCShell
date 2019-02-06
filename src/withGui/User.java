96ba5775c61d49dea38762d3c7fe0a83c60af7fe
Administrator <Administrator@USERUQI-GVQBAVS>
2019-02-06 09:20:17 +0800

    增加了websocket，与MCShell-html进行交互

---
+++ src/withGui/User.java
@@ -0,0 +1,5 @@
+package withGui;
+
+public class User {
+	public boolean yanzheng=true;
+}
---
+++ src/withGui/cfg.java
@@ -0,0 +1,10 @@
+package withGui;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class cfg {
+	public static final String var="MCShell-0.2";
+	public static Map<String,User> user=new HashMap<String,User>();
+	public static String key="123456789";
+}
